68cdaae7bc3f31f25ab3b179a0df6a5d779b999b
David Stump <david@davidstump.net>
2013-01-13 13:41:51 -0500

    Comment timesheet methods

--- lib/mytime/timesheet.rb
+++ lib/mytime/timesheet.rb
@@ -1,19 +1,27 @@
 module Mytime
   extend self
 
+  # Return log of git commits from today
+  #
   def status
     user = `git config --get user.name`
     `git log --oneline --author='#{user}' --since='6am'`
   end
 
+  # Send a custom time entry to API with string message
+  #
+  # Option:
+  #   message: Optional custom message to send with time entry
+  #
   def commit(message = "")
     puts "Submitting: #{message}"
-    puts Client.save(message)
+    puts Client.submit(message)
   end
 
+  # Send git commit log as timesheet entry to client
+  #
   def push
-    message = status.gsub(/\\n/, "\n\n")
-    puts Client.save(message)
+    puts Client.submit(status)
   end
 
 end
