0ccafd3f21ffc113fa16faeb2fef7aa6b497095e
David Stump <david@davidstump.net>
2013-01-13 13:41:06 -0500

    Comment client methods

--- lib/mytime/client.rb
+++ lib/mytime/client.rb
@@ -3,8 +3,28 @@ module Mytime
   module Client
     extend self
 
-    def save(message = "")
-      account = Mytime.get_account_details    
+    # Get project data from client API
+    #
+    # Option:
+    #   project_id: Optional project_id to restrict data returned
+    #
+    def project(project_id = nil)
+      account = config_details    
+      c = FreshBooks::Client.new(account["account"], account["token"])
+      if project_id.nil?
+        c.project.list["projects"]
+      else
+        c.project.get :project_id => project_id
+      end
+    end
+
+    # Submit new time entry to client API
+    #
+    # Option:
+    #   message: Optional message to send with time entry
+    #
+    def submit(message = "")
+      account = config_details  
       c = FreshBooks::Client.new(account["account"], account["token"])
       entry = c.time_entry.create(
         :time_entry => {
@@ -18,6 +38,11 @@ module Mytime
       return validate(entry)
     end
 
+    # Validate time entry to establish API success or failure
+    #
+    # Option
+    #   entry: Required time entry parameter to validate
+    #
     def validate(entry)
       return "Oops. Please try resubmitting your time or checking accout details!" unless entry.any?
       if entry["time_entry_id"]
