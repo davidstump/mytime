7adeb7011596b95919e5babdc23a090b6bf74f9b
David Stump <david@davidstump.net>
2013-01-13 10:32:25 -0500

    Add status command that returns formatted log of commits from today

--- lib/mytime/timesheet.rb
+++ lib/mytime/timesheet.rb
@@ -1,6 +1,11 @@
 module Mytime
   extend self
 
+  def status
+    user = `git config --get user.name`
+    system("git log --oneline --author='#{user}' --since='6am'")
+  end
+
   def commit(message = "")
     puts "added: #{message.to_s}"
     account = self.get_account_details    
