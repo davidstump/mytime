39f8bacf25fb2f492ea03c72fedb8f38af5f9951
David Stump <david@davidstump.net>
2013-01-13 13:41:30 -0500

    Comment setup methods

--- lib/mytime/setup.rb
+++ lib/mytime/setup.rb
@@ -4,13 +4,35 @@ module Mytime
   # Create .mytime file in the profile directory 
   #   and insert the user account and Freshbooks token
   #
-  def init
-    puts "What is your Freshbooks account url?" 
-    account = STDIN.gets.chomp
+  def setup
+    puts "What is your Freshbooks account name?" 
+    account_name = STDIN.gets.chomp
     puts "What is your Freshbooks token?" 
     token = STDIN.gets.chomp
-    contents = ["account" => account, "token" => token]
-    self.save(contents)
+    account = Hash.new
+    account["account"] = "#{account_name}.freshbooks.com"
+    account["token"] = token
+    self.save(account)
+  end
+
+  # Setup .mytime file to include project specific data
+  #
+  def init
+    puts "Choose Project:"
+    projects = Client.project["project"]
+    projects.each do |project|
+      puts "#{project['project_id']}: #{project['name']}"
+    end
+    project_id = STDIN.gets.chomp
+
+    project = Client.project(project_id)["project"]
+    task_id = project["tasks"]["task"][0]["task_id"]
+
+    project_details = Hash.new
+    project_details["project_path"] = Dir.pwd
+    project_details["project_id"] = project_id
+    project_details["task_id"] = task_id
+    self.add(project_details)
   end
 
   # Set configuration variables to values passed in the command line options
@@ -21,6 +43,7 @@ module Mytime
       opts.separator "mytime uses git to log your time\n\n"
       opts.separator "Commands:"
       opts.separator "  status    Prints formatted commit log from today"
+      opts.separator "  setup     Sets up mytime authorization information"
       opts.separator "  init      Sets up mytime for this project"
       opts.separator "  commit    Creates a custom timesheet entry"
       opts.separator "  push      Saves timesheet entry with git commit log"
