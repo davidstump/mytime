a1d34f681f2148f8ef7c21c6cd0361884449315a
David Stump <david@davidstump.net>
2013-01-13 11:23:17 -0500

    Move logic relating to external client to submodule

---
+++ lib/mytime/client.rb
@@ -0,0 +1,32 @@
+module Mytime
+
+  module Client
+    extend self
+
+    def save(message = "")
+      account = Mytime.get_account_details    
+      c = FreshBooks::Client.new(account["account"], account["token"])
+      entry = c.time_entry.create(
+        :time_entry => {
+          project_id: 1,
+          task_id: 1,
+          hours: 4.5,
+          notes: message.to_s,
+          date: Date.today.to_s
+        }
+      )
+      return validate(entry)
+    end
+
+    def validate(entry)
+      return "Oops. Please try resubmitting your time or checking accout details!" unless entry.any?
+      if entry["time_entry_id"]
+        return "Timesheet Submitted"
+      else
+        return "Oops. An error was encountered: #{entry}"
+      end
+    end
+    
+  end
+
+end
